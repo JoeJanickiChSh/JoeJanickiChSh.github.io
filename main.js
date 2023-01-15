3f4671260d65a8935e46ac33669513f13c007307
CupOfJoeCode <therealjoe2794@gmail.com>
2023-01-15 13:42:37 -0500

    Add more options

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
  *
- * TEAM|LEFT AUTO|DOCKED AND ENGAGED|DOCKED AND NOT ENGAGED|NUMLINKS
+ * TEAM|MATCH|LEFT AUTO|DOCKED AND ENGAGED|DOCKED AND NOT ENGAGED|NUMLINKS|NAME|
  *
  */
 
@@ -13,7 +13,15 @@ const loadbool = id => document.getElementById(id).checked * 1;
 
 setInterval(() => {
 
-  format = `${loadval('team-number')}|${loadbool('left-auto')}|${loadbool('docked-engaged')}|${loadbool('docked')}|${loadval('links')}`
+  format = `${loadval('team-number')}|`
+  format += `${loadval('match')}|`
+  format += `${loadbool('left-auto')}|`
+  format += `${loadbool('docked-engaged')}|`
+  format += `${loadbool('docked')}|`
+  format += `${loadval('links')}|`
+  format += `${loadval('name')}|`
+
+
 
   qrcode.clear(); // clear the code.
   qrcode.makeCode(format); // make another code.
